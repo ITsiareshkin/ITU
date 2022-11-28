3d23375ee8c126cff57e8185275547d1c8cd24e7
SadovskyD <73181139+SadovskyD@users.noreply.github.com>
2022-11-28 18:26:52 +0100

    Update views.py

--- saveanimallife/shelter/views.py
+++ saveanimallife/shelter/views.py
@@ -359,7 +359,7 @@ class ShowUsers(UserPassesTestMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = "User page"
+        context['title'] = "Users"
         context['menu'] = menu
         return context
 
